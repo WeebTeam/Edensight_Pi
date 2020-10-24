d2e2bcd56a244b2dfe1dfcd0c5ba21f00d485695
Bryan Neo <bryanncy1999@gmail.com>
2020-10-24 17:10:11 +0100

    REFACTOR: rewrite code to use bleak directly instead of relying on circuitpython blinka uses asyncio so supports multiple devices
    should no longer have unexplainable hangs while scanning for devices

--- edensight_node.py
+++ edensight_node.py
@@ -7,33 +7,157 @@ Connects to the pulse oximeter data (only vital signs we could afford to monitor
 to our backend server for monitoring
 
 For the device: BerryMedical BM1000C
+
+Dependencies:
+bleak
+requests 
 """
-# Credits to Adafruit for https://github.com/adafruit/Adafruit_CircuitPython_BLE_BerryMed_Pulse_Oximeter
-# which is the library that we are using here
-# The sensor updates the readings at 100Hz.
 
 import sys
-import time
-import board
-import busio
-import digitalio
-import _bleio
-import adafruit_ble
-from adafruit_ble.advertising.standard import Advertisement
-from adafruit_ble.services.standard.device_info import DeviceInfoService
-from adafruit_ble_berrymed_pulse_oximeter import BerryMedPulseOximeterService
-
+import asyncio
 import requests
 from requests.auth import HTTPBasicAuth
+from bleak import BleakScanner
+from bleak import BleakClient
 
-# PyLint can't find BLERadio for some reason so special case it here.
-ble = adafruit_ble.BLERadio()  # pylint: disable=no-member
+####### variables declaration ###########
+berryMedCharacteristicUUID = "49535343-1e4d-4bd9-ba61-23c647249616"
 
-pulse_ox_connection = None
-pulse_mac_addr = None
-last_connecting_time = None
+# for storing vital signs data before sending to server
+# key value with key being mac address of device
+# value being array of dictionaries of sensor data
+vitalSignsData = {}
 
-last_sent_time = None
+########### functions/coroutines ###########
+
+# main async function?
+async def run():
+    # we wanna run this forever as we want to keep scanning for devices
+    # but we scan evert 5 secs
+    while True:
+        await scanForBerryMedDevices()
+        await asyncio.sleep(5)
+
+# scan for berrymed sensors
+async def scanForBerryMedDevices():
+    devices = await BleakScanner.discover()
+    print("Scanning for devices...")
+    for d in devices:
+        if d.name.strip("\x00") == "BerryMed":
+            # if device is starts with berrymed, connect to it
+            print("BerryMed device found!")
+            
+            # create a task to handle the device async-ly
+            # loop continues to search for other devices while the task reads data from
+            # the sensor (MULTIPLE DEVICE SUPPORT!!!!)
+            asyncio.create_task(connectToBerryMedDevice(d.address))
+
+
+# connects to the device and start sending data to server
+async def connectToBerryMedDevice(address):
+    print("connecting to device", address)
+    client =  BleakClient(address)
+    
+    def berryMedNotify(sender, data):
+        # use nonlocal to get the address variable of the outer function
+        # this is because the callback function params are expected by start_notify
+        # and cannot be altered. therefore we get the address only and hand that over 
+        # to another function to do the actual parsing
+        nonlocal address
+        parseBerryMedData(address, data)
+    
+    try:
+        await client.connect()
+        
+        # we loop forever as we want data from the sensor forever (until program quits or 
+        # device is disconnected)
+        while True:
+            # start notify to get data from the sensor
+            # we only wait 0.1 before closing as we dont need so much data (wastes processing anyways)
+            await client.start_notify(berryMedCharacteristicUUID, berryMedNotify)
+            await asyncio.sleep(0.1)
+            await client.stop_notify(berryMedCharacteristicUUID)
+            
+            # wait abit for any callbacks to finish
+            await asyncio.sleep(0.1)
+            sendDataToServer(address)
+            
+            # after stop notify we wait for 5 seconds before getting data from sensor again
+            await asyncio.sleep(4.8)
+            
+    except Exception as e:
+       # print("------------------- EXCEPTION OWO ---------------")
+        #print(e)
+    finally:
+        print("disconnecting from", address)
+        await client.disconnect()
+
+# parse data from BerryMed vital signs monitor
+# at this point it only parses pulse rate and spo2
+def parseBerryMedData(address, data):
+    count = 0
+    packet = bytearray()
+    packets = []
+
+    for byte in data:
+        # each packet is 5 bytes long
+        # we split them into an array of bytearrays 5 byte wide
+        packet.append(byte)
+        count += 1
+        
+        if count == 5:
+            packets.append(packet)
+            packet = bytearray()
+            count = 0
+    
+    # source for data packet info: https://raw.githubusercontent.com/zh2x/BCI_Protocol/master/BCI%20Protocol%20V1.2.pdf
+    # go through all the collected data and parse the vital signs
+    for p in packets:
+        # we only need pulse and spo2 so only getting them both
+        pulse = p[3] + (p[2] & (1 << 7))
+        spo2 = p[4]
+        
+        # according to pdf these 2 values are invalid
+        # save valid values into the vital signs data array for later processing
+        if pulse != 0xFF and spo2 != 0x7F:
+            if address in vitalSignsData:
+                # if we have previously added data in, just append
+                vitalSignsData[address].append({"pulse": pulse, "spo2": spo2})
+            else:
+                # if not we create new key value with the array
+                vitalSignsData[address] = [{"pulse": pulse, "spo2": spo2}]
+
+
+def sendDataToServer(address):
+    if address in vitalSignsData:
+        dataArray = vitalSignsData[address]
+
+        # average that data and send it    
+        meanData = {"pulse": 0, "spo2": 0}
+        
+        for data in dataArray:
+            meanData["pulse"] += data["pulse"]
+            meanData["spo2"] += data["spo2"]
+        arraySize = len(dataArray)
+        meanData["pulse"] = int(meanData["pulse"]/arraySize)
+        meanData["spo2"] = int(meanData["spo2"]/arraySize)
+        
+        # clear the data array, since we have the mean values now
+        del vitalSignsData[address]
+        
+        #post to server
+        response = requests.post('https://braserver.mooo.com/edensight/api/vitalsigns/add', data = {'macAddr':address.lower(), 'heartRate':meanData["pulse"], 'spO2':meanData["spo2"]}, auth=(backendUname, backendPasswd))
+        
+        print(address, ":", meanData, "|", response.status_code)
+    else:
+        print("no data for", address)
+
+    
+########## end of functions/coroutines declaration #######
+
+
+
+################################## code runs here? ##################################
 
 # print welcome message
 print("Edensight Raspberry Pi Node (Data collection)")
@@ -43,114 +167,22 @@ if len(sys.argv) != 3:
     print("./edensight_node <username> <password>")
     exit()
 
-#get username password from command line arg
-username = sys.argv[1]
-password = sys.argv[2]
+backendUname = sys.argv[1]
+backendPasswd = sys.argv[2]
+
+
+
+####### main ########
+# we just call the run() async function and let it do the rest
 
 try:
-    while True:
-        if pulse_mac_addr is None:
-            print("Scanning for Pulse Oximeter... ")
-            # scan for ble advertisements
-            for advertisement in ble.start_scan(timeout=5):
-                # check if a device is found
-                name = advertisement.complete_name
-                if not name:
-                    # restart loop if name is invalid (no device found)
-                    continue
-                # check if the advertisement is the oximeter we are looking for
-                # "BerryMed" devices may have trailing nulls on their name.
-                if name.strip("\x00") == "BerryMed":
-                    # connect to the device
-                    connecting_count = 0
-                    pulse_ox_connection = ble.connect(advertisement)
-                    pulse_mac_addr = advertisement.address
-                    print("Device found! Address:", pulse_mac_addr.string)
-                    print("Connecting...")
-                    # break out of the loop and go to next part of the code
-                    break
-            ble.stop_scan()
-
-        try:
-            # if we are connected (so if we lose connection we can restart from top)
-            if pulse_ox_connection and pulse_ox_connection.connected:
-                # get the pulse oximeter service (subscribe to the service)
-                pulse_ox_service = pulse_ox_connection[BerryMedPulseOximeterService]
-
-                while pulse_ox_connection.connected:
-                    # receive data from the pulse oximeter
-                    values = pulse_ox_service.values
-                    if values is not None: #if theres value
-                        # unpack the message to 'values' list
-                        valid, spo2, pulse_rate, pleth, finger = values
-
-                        # the valid checking in adafruit's library only checks spO2
-                        # lets check pulse rate and pleth too
-                        if not valid or pulse_rate == 255 or pulse_rate == 0 or pleth > 100 or spo2 < 50:
-                            # ignore the data if invalid
-                            continue
-                        # todo: buffer and average every second
-
-                        #average and send every second
-                        if last_sent_time is None:
-                            last_sent_time = time.time()
-                        else:
-                            current_time = time.time()
-                            #if 5 seconds has passed since last send
-                            if current_time - last_sent_time > 5:
-                                last_sent_time = current_time
-
-                                print(
-                                    "sending data to server... | ",
-                                    "SpO2: {}%  | ".format(spo2),
-                                    "Pulse Rate: {} BPM  | ".format(pulse_rate),
-                                    "Pleth: {}".format(pleth)
-                                )
-
-                                print(requests.post('https://braserver.mooo.com/edensight/api/vitalsigns/add', data = {'macAddr':pulse_mac_addr.string, 'heartRate':pulse_rate, 'spO2':spo2}, auth=(username, password)))
-
-            if last_connecting_time is None:
-                last_connecting_time = time.time()
-            else:
-                connecting_time = time.time()
-                #if 5 seconds has passed since last send
-                if connecting_time - last_connecting_time > 5:
-                    last_connecting_time = connecting_time
-                    #timeout connecting after 5s, try to scan and reconnect again
-                    pulse_mac_addr = None
-                    if pulse_ox_connection and pulse_ox_connection.connected:
-                        pulse_ox_connection.disconnect()
-                        pulse_ox_connection = None
-
-        # todo: properly handle disconnect
-        #       out of range disconnects?
-        #except (_bleio.ConnectionError, AttributeError):
-        except Exception as e:
-            print(e)
-            print("Connection lost.")
-            try:
-                print("Disconnecting...")
-                pulse_ox_connection.disconnect()
-            #except (_bleio.ConnectionError, AttributeError):
-            except Exception as e:
-                print("2: ", e)
-                pass
-            print("Disconnected. Scanning for device again.")
-            pulse_ox_connection = None
-            pulse_mac_addr = None
-        #print("  end of while?")
+    asyncio.run(run())
 
 except KeyboardInterrupt:
     print("\n\nQuitting...")
-    try:
-        #if connected, gracefully disconnect
-        if pulse_ox_connection and pulse_ox_connection.connected:
-            print("Disconnecting...")
-            pulse_mac_addr = None
-            pulse_ox_connection.disconnect()
-            pulse_ox_connection = None
-    except:
-        pass #nothing to do
 
     print("Goodbye!")
     exit()
+    
+######## End of main ###########    
+
